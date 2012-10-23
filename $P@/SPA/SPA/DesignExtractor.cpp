3dff3ed89212ca07d2ddab6abfcd0bbeac884aa7
weixiong <howeixiong@gmail.com>
2012-10-23 20:04:35 +0800

    fixed several bugs in DesignExtractor.

--- $P@/SPA/SPA/DesignExtractor.cpp
+++ $P@/SPA/SPA/DesignExtractor.cpp
@@ -3,179 +3,218 @@
 DesignExtractor::DesignExtractor(CallsTable& ca, ModifiesTable& mo,
 	UsesTable& us, ParentTable& pa, FollowsTable& fo)
 {
-	//c = ca;
-	//m = mo;
-	//u = us;
-	//p = pa;
-	//f = fo;
+	//currently, not going to check nodes if it is of
+	//the correct node type before typecasting
+	//not sure whether to do so at all or not
 
-	//stack<StmtNode*> DFSstack;
-	//stack<int> positionStack;
-	//stack<StmtNode*> nextStmtStack;
-	//stack<StmtNode*> tempStack;
-	//ASTNode* tempNode;
+	c = ca;
+	m = mo;
+	u = us;
+	p = pa;
+	f = fo;
 
-	//ASTNode* programNode = PKB::rootNode; //program
-	//if ((*programNode).getType() != ASTNode::NodeType::Program)
-	//	throw SPAException("Expecting a Program Node");
+	stack<StmtNode*> DFSstack;
+	stack<StmtLstNode*> DFSstmtLstStack;
+	stack<int> positionStack;
+	stack<bool> traversingThenPartOfIfStack;
+	stack<StmtNode*> tempStack;
+	stack<ExprNode*> exprStack;
+	ASTNode* tempASTNode;
+	StmtNode* tempStmtNode;
 
-	////just going to handle one procedure for now
-	//ASTNode* procedureNode = (*programNode).getChild(0); //procedure
-	//if ((*procedureNode).getType() != ASTNode::NodeType::Procedure)
-	//	throw SPAException("Expecting a Procedure Node");
-	//PROC currentProc = (*procedureNode).getValue();
+	ASTNode* programNode = PKB::rootNode; //program
+	/*if ((*programNode).getType() != ASTNode::NodeType::Program)
+		throw SPAException("Expecting a Program Node");*/
 
-	//ASTNode* stmtListNode = (*procedureNode).getChild(0); //stmtList
-	//if ((*stmtListNode).getType() != ASTNode::NodeType::StmtLst)
-	//	throw SPAException("Expecting a StmtLst Node");
+	//just going to handle one procedure for now
+	ASTNode* procedureNode = (*programNode).getChild(0); //procedure
+	/*if ((*procedureNode).getType() != ASTNode::NodeType::Procedure)
+		throw SPAException("Expecting a Procedure Node");*/
+	PROC currentProc = (*procedureNode).getValue();
 
-	//if (!(*stmtListNode).isHasChildren())
-	//	return;
+	tempASTNode = (*procedureNode).getChild(0); //stmtList
+	/*if ((*tempNode).getType() != ASTNode::NodeType::StmtLst)
+		throw SPAException("Expecting a StmtLst Node");*/
+	StmtLstNode* firstLevelStmtListNode = (StmtLstNode*) tempASTNode;
+	/*if (!(*firstLevelStmtListNode).isHasChildren())
+		return;*/
 
-	//tempNode = (*stmtListNode).getChild(0); //first child
-	//if ((*tempNode).getType() != ASTNode::NodeType::Stmt)
-	//	throw SPAException("Expecting a Stmt Node");
-	//StmtNode* currentStmtNode = (StmtNode*) tempNode;
-	//STMT currentStmtNumber = (*currentStmtNode).getStmtNumber();
-	//int rootPosition = 0;
-	//
-	//while (true) {
-	//	switch ((*currentStmtNode).getType()) {
-	//	case ASTNode::NodeType::Assign: {
-	//		//todo: check all nodes if its of correct node type before typecasting
-	//		ExprNode* modifiesVarNode = (ExprNode*) (*currentStmtNode).getChild(0);
-	//		VAR modifiesVar = (*modifiesVarNode).getValue();
+	tempASTNode = (*firstLevelStmtListNode).getChild(0); //first child
+	/*if ((*tempNode).getType() != ASTNode::NodeType::Stmt)
+		throw SPAException("Expecting a Stmt Node");*/
+	StmtNode* firstLevelStmtNode = (StmtNode*) tempASTNode;
+	int firstLevelPosition = 0;
 
-	//		m.insertProcModifies(currentProc, modifiesVar);
-	//		m.insertStmtModifies(currentStmtNumber, modifiesVar);
-	//		while (!DFSstack.empty()) {
-	//			StmtNode* tempNode = DFSstack.top();
-	//			DFSstack.pop();
-	//			STMT tempNumber = (*tempNode).getStmtNumber();
-	//			m.insertStmtModifies(tempNumber, modifiesVar);
-	//			tempStack.push(tempNode);
-	//		}
-	//		while (!tempStack.empty()) {
-	//			StmtNode* tempNode = tempStack.top();
-	//			tempStack.pop();
-	//			DFSstack.push(tempNode);
-	//		}
+	StmtLstNode* currentStmtListNode = firstLevelStmtListNode;
+	StmtNode* currentStmtNode = firstLevelStmtNode;
+	STMT currentStmtNumber = (*currentStmtNode).getStmtNumber();
+	int currentPosition = 0;
+	
+	while (true) {
+		switch ((*currentStmtNode).getType()) {
+		case ASTNode::NodeType::Assign: {
+			ExprNode* modifiesVarNode = (ExprNode*) (*currentStmtNode).getChild(0);
+			VAR modifiesVar = (*modifiesVarNode).getValue();
 
-	//		//check rhs for all uses relationships
-	//		stack<ExprNode*> exprStack;
-	//		exprStack.push( (ExprNode*) (*currentStmtNode).getChild(1) );
-	//		do {
-	//			ExprNode* exprNode = exprStack.top();
-	//			exprStack.pop();
-	//			if ((*exprNode).getType() == ASTNode::NodeType::Variable) {
-	//				VAR usesVar = (*exprNode).getValue(); 
-	//				u.insertProcUses(currentProc, usesVar);
-	//				u.insertStmtUses(currentStmtNumber, usesVar);
-	//				while (!DFSstack.empty()) {
-	//					StmtNode* tempNode = DFSstack.top();
-	//					DFSstack.pop();
-	//					STMT tempNumber = (*tempNode).getStmtNumber();
-	//					u.insertStmtUses(tempNumber, usesVar);
-	//					tempStack.push(tempNode);
-	//				}
-	//				while (!tempStack.empty()) {
-	//					StmtNode* tempNode = tempStack.top();
-	//					tempStack.pop();
-	//					DFSstack.push(tempNode);
-	//				}
-	//			}
-	//			else if ((*exprNode).isHasChildren()) {
-	//				exprNode = (ExprNode*) (*exprNode).getChild(0);
-	//				exprStack.push(exprNode);
-	//				int index = 0;
-	//				while ((*exprNode).isHasRightSibling()) {
-	//					exprNode = (ExprNode*) (*exprNode).getChild(++index);
-	//					exprStack.push(exprNode);
-	//				}
-	//			}
-	//		} while (!exprStack.empty());
-	//		break; }
+			m.insertProcModifies(currentProc, modifiesVar);
+			m.insertStmtModifies(currentStmtNumber, modifiesVar);
+			while (!DFSstack.empty()) {
+				tempStmtNode = DFSstack.top();
+				DFSstack.pop();
+				STMT tempNumber = (*tempStmtNode).getStmtNumber();
+				m.insertStmtModifies(tempNumber, modifiesVar);
+				tempStack.push(tempStmtNode);
+			}
+			while (!tempStack.empty()) {
+				tempStmtNode = tempStack.top();
+				tempStack.pop();
+				DFSstack.push(tempStmtNode);
+			}
 
-	//	case ASTNode::NodeType::Call: {
-	//		c.insertCalls(currentProc, (*currentStmtNode).getValue());
-	//		/*
-	//		not yet handling the case with multiple procedures
-	//		but the idea is to save the current DFSstack contents into another stack/queue
-	//		when ALL the procedures have been completed
-	//		then slowly add back all the modifies and uses information in.
-	//		Note: must wait till all the procedures have been checked to ensure total coverage.
-	//		//*/
-	//		break; }
+			//check rhs for all uses relationships
+			if (!exprStack.empty())
+				throw new SPAException("Huh, how come stack not empty?"); 
+			exprStack.push( (ExprNode*) (*currentStmtNode).getChild(1) ); //rhs of assign
+			do {
+				ExprNode* exprNode = exprStack.top();
+				exprStack.pop();
+				if ((*exprNode).getType() == ASTNode::NodeType::Variable) {
+					VAR usesVar = (*exprNode).getValue(); 
+					u.insertProcUses(currentProc, usesVar);
+					u.insertStmtUses(currentStmtNumber, usesVar);
+					while (!DFSstack.empty()) {
+						tempStmtNode = DFSstack.top();
+						DFSstack.pop();
+						STMT tempNumber = (*tempStmtNode).getStmtNumber();
+						u.insertStmtUses(tempNumber, usesVar);
+						tempStack.push(tempStmtNode);
+					}
+					while (!tempStack.empty()) {
+						tempStmtNode = tempStack.top();
+						tempStack.pop();
+						DFSstack.push(tempStmtNode);
+					}
+				} //a variable would not have any children
+				else if ((*exprNode).isHasChildren()) {
+					exprStack.push((ExprNode*) (*exprNode).getChild(0));
+					exprStack.push((ExprNode*) (*exprNode).getChild(1));
+				}
+			} while (!exprStack.empty());
+			break; }
 
-	//	case ASTNode::NodeType::While:
-	//	case ASTNode::NodeType::If: {
-	//		if ((*currentStmtNode).getType() == ASTNode::NodeType::If)
-	//			nextStmtStack.push((StmtNode*) (*currentStmtNode).getChild(2));
-	//		nextStmtStack.push((StmtNode*) (*currentStmtNode).getChild(1));
+		case ASTNode::NodeType::Call: {
+			c.insertCalls(currentProc, (*currentStmtNode).getValue());
+			/*
+			not yet handling the case with multiple procedures
+			but the idea is to save the current DFSstack contents into another stack/queue
+			when ALL the procedures have been completed
+			then slowly add back all the modifies and uses information in.
+			Note: must wait till all the procedures have been checked to ensure total coverage.
+			//*/
+			break; }
 
-	//		ExprNode* usesVarNode = (ExprNode*) (*currentStmtNode).getChild(0);
-	//		VAR usesVar = (*usesVarNode).getValue(); 
-	//		u.insertProcUses(currentProc, usesVar);
-	//		u.insertStmtUses(currentStmtNumber, usesVar);
-	//		while (!DFSstack.empty()) {
-	//			StmtNode* tempNode = DFSstack.top();
-	//			DFSstack.pop();
-	//			STMT tempNumber = (*tempNode).getStmtNumber();
-	//			u.insertStmtUses(tempNumber, usesVar);
-	//			tempStack.push(tempNode);
-	//		}
-	//		while (!tempStack.empty()) {
-	//			StmtNode* tempNode = tempStack.top();
-	//			tempStack.pop();
-	//			DFSstack.push(tempNode);
-	//		}
-	//		break; }
-	//	default: throw SPAException("Unhandled Node type");
-	//	}
+		case ASTNode::NodeType::While:
+		case ASTNode::NodeType::If: {
+			ExprNode* usesVarNode = (ExprNode*) (*currentStmtNode).getChild(0);
+			VAR usesVar = (*usesVarNode).getValue(); 
+			u.insertProcUses(currentProc, usesVar);
+			u.insertStmtUses(currentStmtNumber, usesVar);
+			while (!DFSstack.empty()) {
+				tempStmtNode = DFSstack.top();
+				DFSstack.pop();
+				STMT tempNumber = (*tempStmtNode).getStmtNumber();
+				u.insertStmtUses(tempNumber, usesVar);
+				tempStack.push(tempStmtNode);
+			}
+			while (!tempStack.empty()) {
+				tempStmtNode = tempStack.top();
+				tempStack.pop();
+				DFSstack.push(tempStmtNode);
+			}
+			break; }
+		default: throw SPAException("Unhandled Node type");
+		}
 
-	//	//add Parent relationship if current node is a child
-	//	if (!DFSstack.empty()) {
-	//		StmtNode* tempNode = DFSstack.top();
-	//		STMT parentStmtNumber = (*tempNode).getStmtNumber();
-	//		p.insertParent(parentStmtNumber, currentStmtNumber);
-	//	}
+		//go to the next stmt.
+		//go down if can go down (container statement)
+		if ((*currentStmtNode).getType() == ASTNode::NodeType::While
+			|| (*currentStmtNode).getType() == ASTNode::NodeType::If) {
+				DFSstack.push(currentStmtNode);
+				DFSstmtLstStack.push(currentStmtListNode);
+				positionStack.push(currentPosition);
 
-	//	//add Follows relationship if current node has a right sibling
-	//	if ((*currentStmtNode).isHasRightSibling()) {
-	//		StmtNode* rightSibling;
-	//		ASTNode* parentNode;
-	//		int pointer;
-	//		if (DFSstack.empty()) {
-	//			parentNode = stmtListNode;
-	//			pointer = rootPosition;
-	//		} else {
-	//			parentNode = DFSstack.top();
-	//			pointer = positionStack.top();
-	//		}
-	//		rightSibling = (StmtNode*) (*parentNode).getChild(pointer + 1);
-	//		STMT rightSiblingNumber = (*rightSibling).getStmtNumber();
-	//		f.insertFollows(currentStmtNumber, rightSiblingNumber);
-	//	}
+				if ((*currentStmtNode).getType() == ASTNode::NodeType::If)
+					traversingThenPartOfIfStack.push(true);
+				//indicates is in the 'then' part of the if statement
 
-	//	//go to the next stmt. choose from nextStmtStack if possible
-	//	if (nextStmtStack.empty()) {
-	//		ASTNode* parentNode;
-	//		int pointer;
-	//		if (DFSstack.empty()) {
-	//			parentNode = stmtListNode;
-	//			pointer = rootPosition;
-	//		} else {
-	//			parentNode = DFSstack.top();
-	//			pointer = positionStack.top();
-	//		}
-	//		currentStmtNode = (StmtNode*) (*parentNode).getChild(pointer + 1);
-	//	} else {
-	//		DFSstack.push(currentStmtNode);
-	//		positionStack.push(0);
-	//		currentStmtNode = nextStmtStack.top();
-	//		nextStmtStack.pop();
-	//	}
-	//}
+				currentStmtListNode = (StmtLstNode*) (*currentStmtNode).getChild(1);
+				currentStmtNode = (StmtNode*) (*currentStmtListNode).getChild(0);
+				STMT newStmtNumber = (*currentStmtNode).getStmtNumber();
+				currentPosition = 0;
+				
+				//add Parent and Follows relationship for all the children
+				StmtNode* olderChild = currentStmtNode;
+				STMT olderChildNumber = newStmtNumber;
+				StmtNode* youngerChild;
+				STMT youngerChildNumber;
+				p.insertParent(currentStmtNumber, olderChildNumber);
+				for (int i = 1; i < (*currentStmtListNode).getSize(); i++) {
+					youngerChild = (StmtNode*) (*currentStmtListNode).getChild(i);
+					youngerChildNumber = (*youngerChild).getStmtNumber();
+					p.insertParent(currentStmtNumber, youngerChildNumber);
+					f.insertFollows(olderChildNumber, youngerChildNumber);
+					olderChild = youngerChild;
+					olderChildNumber = youngerChildNumber;
+				}
+
+				currentStmtNumber = newStmtNumber;
+		} else {
+			//go right if can go right, if cannot, go up until can go right
+			bool notYetGotNextChild = true;
+			while (notYetGotNextChild) {
+				if (DFSstack.empty()) {
+					if (firstLevelPosition + 1 < (*firstLevelStmtListNode).getSize()) {
+						currentStmtNode = (StmtNode*) (*firstLevelStmtNode).getChild(++firstLevelPosition);
+						//currentPosition++; //not relevant for first level traversing
+						notYetGotNextChild = false;
+					} else
+						return; //the end!
+				} else {
+					if (currentPosition + 1 < (*currentStmtListNode).getSize()) { //try right
+						currentStmtNode = (StmtNode*) (*currentStmtListNode).getChild(++currentPosition);
+						currentStmtNumber = (*currentStmtNode).getStmtNumber();
+
+						notYetGotNextChild = false;
+					} else { //must go up
+						StmtNode* parentNode = DFSstack.top();
+					
+						if ((*parentNode).getType() == ASTNode::NodeType::If &&
+							traversingThenPartOfIfStack.top()) { //go to 'else' part of if
+								traversingThenPartOfIfStack.pop();
+								traversingThenPartOfIfStack.push(false);
+
+								currentStmtListNode = (StmtLstNode*) (*parentNode).getChild(2);
+								currentStmtNode = (StmtNode*) (*currentStmtListNode).getChild(0);
+								currentStmtNumber = (*currentStmtNode).getStmtNumber();
+								currentPosition = 0;
+
+								notYetGotNextChild = false;
+						} else {
+							if ((*parentNode).getType() == ASTNode::NodeType::If)
+								traversingThenPartOfIfStack.pop(); //was at 'else' part of if
+
+							currentStmtNode = parentNode;
+							currentStmtListNode = DFSstmtLstStack.top();
+							currentPosition = positionStack.top();
+							
+							DFSstack.pop();
+							DFSstmtLstStack.pop();
+						}
+					}
+				}
+			}
+		}
+	}
 }
 
 

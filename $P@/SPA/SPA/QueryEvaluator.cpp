3c339d3aeb6997b4da9a36ce71838dcd0ee44085
Allan Chong <kw_chong@nus.edu.sg>
2012-10-29 17:00:11 +0800

    Simon, please use this and continue
    Bug when integrating QueryEvaulator, Brackets were not matched correctly in the code..
    
    There is no brackets in case do not put in

--- $P@/SPA/SPA/QueryEvaluator.cpp
+++ $P@/SPA/SPA/QueryEvaluator.cpp
@@ -43,203 +43,175 @@ vector<string> QueryEvaluator::returnAnswer() {
 	}
 	QueryTreeBuilder querytreebuilder;
 	vector<vector<QueryTreeNode*>> querytree = querytreebuilder.getQueryTree();
+
 	for (int i = 0; i < querytree.size(); i++) {
 		vector<QueryTreeNode*> querycluster = querytree[i];
 		QueryTreeNode* currentnode = querycluster[0];
 		QueryTreeNode::QTNodeType nodetype = currentnode->getNodeType();
-		
 		if (nodetype == QueryTreeNode::Relationship) {
 			
 			QueryRelNode* rnode = (QueryRelNode*) currentnode;
 			QueryEnums::QueryRel rtype = rnode->getRelationshipType();
-			
-			switch(rtype) {
-			
-			case QueryEnums::Calls: {
-				
+			if (nodetype == QueryTreeNode::Relationship) {
+				QueryRelNode* rnode = (QueryRelNode*) currentnode;
+				QueryEnums::QueryRel rtype = rnode->getRelationshipType();
 				QueryEnums::QueryVar firsttype = rnode->getFirstVariableType();
 				QueryEnums::QueryVar secondtype = rnode->getSecondVariableType();
-				
-				if (firsttype != QueryEnums::QueryVar::Procedure || secondtype != QueryEnums::QueryVar::Procedure) {
-					throw new SPAException("Hanwei, you screwed up!");
-					querycluster.pop_back();
-					vector<pair<string, string>> newans;
-					newans.push_back(make_pair("false", "false"));
-					QueryProjectNode* newprojnode = new QueryProjectNode(newans);
-					querycluster.push_back(newprojnode);
-					vector<string> ans;
-					ans.push_back("false");
-					return ans;
-				} else {
-					querycluster.pop_back();
-					vector<pair<string, string>> newans;
-					for (int i = 0; i < PKB::procedures.getSize(); i++) {
-						PROCIndex index = (PROCIndex) i;
-						string curr_name = PKB::procedures.getPROCName(index);
-						vector<PROC> getcalledby = PKB::calls.getCalledBy(index);
-						if (!getcalledby.empty()) {
-							for (int j = 0; j < getcalledby.size(); j++) {
-								PROCIndex index2 = (PROCIndex) j;
-								string next_name = PKB::procedures.getPROCName(index2);
-								newans.push_back(make_pair(curr_name, next_name));
-							}
-						}
-					}
-					QueryProjectNode* newprojnode = new QueryProjectNode(newans);
-					querycluster.push_back(newprojnode);
-				}
-				break; }
-			case QueryEnums::CallsStar: {
-				
-				QueryEnums::QueryVar firsttype = rnode->getFirstVariableType();
-				QueryEnums::QueryVar secondtype = rnode->getSecondVariableType();
-				
-				if (firsttype != QueryEnums::QueryVar::Procedure || secondtype != QueryEnums::QueryVar::Procedure) {
-					querycluster.pop_back();
-					vector<pair<string, string>> newans;
-					newans.push_back(make_pair("false", "false"));
-					QueryProjectNode* newprojnode = new QueryProjectNode(newans);
-					querycluster.push_back(newprojnode);
-				} else {
-					querycluster.pop_back();
-					vector<pair<string, string>> newans;
-					for (int i = 0; i < PKB::procedures.getSize(); i++) {
-						PROCIndex index = (PROCIndex) i;
-						string curr_name = PKB::procedures.getPROCName(index);
-						vector<PROC> getcalledbystar = PKB::calls.getCalledByStar(index);
-						if (!getcalledbystar.empty()) {
-							for (int j = 0; j < getcalledbystar.size(); j++) {
-								PROCIndex index2 = (PROCIndex) j;
-								string next_name = PKB::procedures.getPROCName(index2);
-								newans.push_back(make_pair(curr_name, next_name));
-							}
-						}
-					QueryProjectNode* newprojnode = new QueryProjectNode(newans);
-					querycluster.push_back(newprojnode);
-				}
-				break; }
-			
-			case QueryEnums::Follows: {
-					
-				QueryEnums::QueryVar firsttype = rnode->getFirstVariableType();
-				QueryEnums::QueryVar secondtype = rnode->getSecondVariableType();
-				
-				if (firsttype != QueryEnums::QueryVar::Stmt || secondtype != QueryEnums::QueryVar::Stmt) {
-					querycluster.pop_back();
-					vector<pair<string, string>> newans;
-					newans.push_back(make_pair("false", "false"));
-					QueryProjectNode* newprojnode = new QueryProjectNode(newans);
-					querycluster.push_back(newprojnode);
-				} else {
-					int maxlines = PKB::maxProgLines;
-					querycluster.pop_back();
-					vector<pair<int, int>> newans;
-					for (int i = 0; i < maxlines; i++) {
-						STMT index = i + 1;
-						int followed = PKB::follows.getFollowsBy(index);
-						if (followed != -1) {
-							newans.push_back(make_pair(index, followed));
-						}
-					}
-					QueryProjectNode* newprojnode = new QueryProjectNode(newans);
-					querycluster.push_back(newprojnode);
-				}
-				break; }
-			
-			case QueryEnums::FollowsStar: {
-					
-				QueryEnums::QueryVar firsttype = rnode->getFirstVariableType();
-				QueryEnums::QueryVar secondtype = rnode->getSecondVariableType();
-				
-				if (firsttype != QueryEnums::QueryVar::Stmt || secondtype != QueryEnums::QueryVar::Stmt) {
-					querycluster.pop_back();
-					vector<pair<string, string>> newans;
-					newans.push_back(make_pair("false", "false"));
-					QueryProjectNode* newprojnode = new QueryProjectNode(newans);
-					querycluster.push_back(newprojnode);
-				} else {
-					int maxlines = PKB::maxProgLines;
-					querycluster.pop_back();
-					vector<pair<int, int>> newans;
-					for (int i = 0; i < maxlines; i++) {
-						int index = i + 1;
-						vector<STMT> getfollowsbystar = PKB::follows.getFollowsStarBy(index);
-						if (!getfollowsbystar.empty()) {
-							for (int j = 0; j < getfollowsbystar.size(); j++) {
-								newans.push_back(make_pair(index, getfollowsbystar[j]));
-							}
-						}
-					}
-					QueryProjectNode* newprojnode = new QueryProjectNode(newans);
-					querycluster.push_back(newprojnode);
-				}
-				break; }
 
-			case QueryEnums::Parent: {
+			switch(rtype) {
+				case QueryEnums::Calls: 
+					if (firsttype != QueryEnums::QueryVar::Procedure || secondtype != QueryEnums::QueryVar::Procedure) {
+						throw new SPAException("Hanwei, you screwed up!");
+					} else {
+						querycluster.pop_back();
+						vector<pair<string, string>> newans;
+						for (int i = 0; i < PKB::procedures.getSize(); i++) {
+							PROCIndex index = (PROCIndex) i;
+							string curr_name = PKB::procedures.getPROCName(index);
+							vector<PROC> getcalledby = PKB::calls.getCalledBy(index);
+							if (!getcalledby.empty()) {
+								for (int j = 0; j < getcalledby.size(); j++) {
+									PROCIndex index2 = (PROCIndex) j;
+									string next_name = PKB::procedures.getPROCName(index2);
+									newans.push_back(make_pair(curr_name, next_name));
+								}
+							}
+						}
+						QueryProjectNode* newprojnode = new QueryProjectNode(newans);
+						querycluster.push_back(newprojnode);
+					}
+					break; 
+				case QueryEnums::CallsStar: 
+						if (firsttype != QueryEnums::QueryVar::Procedure || secondtype != QueryEnums::QueryVar::Procedure) {
+							throw new SPAException("Hanwei, you screwed up!");
+						} else {
+					
+							querycluster.pop_back();
+							vector<pair<string, string>> newans;
+					
+							for (int i = 0; i < PKB::procedures.getSize(); i++) {
+								PROCIndex index = (PROCIndex) i;
+								string curr_name = PKB::procedures.getPROCName(index);
+								vector<PROC> getcalledbystar = PKB::calls.getCalledByStar(index);
+								if (!getcalledbystar.empty()) {
+									for (int j = 0; j < getcalledbystar.size(); j++) {
+										PROCIndex index2 = (PROCIndex) j;
+										string next_name = PKB::procedures.getPROCName(index2);
+										newans.push_back(make_pair(curr_name, next_name));
+									}
+								}
+							QueryProjectNode* newprojnode = new QueryProjectNode(newans);
+							querycluster.push_back(newprojnode);
+				
+							}
+						}
+						break; 
 						
-				QueryEnums::QueryVar firsttype = rnode->getFirstVariableType();
-				QueryEnums::QueryVar secondtype = rnode->getSecondVariableType();
+					case QueryEnums::Follows: 
+						if (firsttype != QueryEnums::QueryVar::Stmt || secondtype != QueryEnums::QueryVar::Stmt) {
+							querycluster.pop_back();
+							vector<pair<string, string>> newans;
+							newans.push_back(make_pair("false", "false"));
+							QueryProjectNode* newprojnode = new QueryProjectNode(newans);
+							querycluster.push_back(newprojnode);
 				
-				if (firsttype != QueryEnums::QueryVar::Stmt || secondtype != QueryEnums::QueryVar::Stmt) {
-					querycluster.pop_back();
-					vector<pair<string, string>> newans;
-					newans.push_back(make_pair("false", "false"));
-					QueryProjectNode* newprojnode = new QueryProjectNode(newans);
-					querycluster.push_back(newprojnode);
-				} else {
-					int maxlines = PKB::maxProgLines;
-					querycluster.pop_back();
-					vector<pair<int, int>> newans;
-					for (int i = 0; i < maxlines; i++) {
-						STMT index = i + 1;
-						vector<STMT> children = PKB::parent.getChildren(index);
-						if (!children.empty()) {
-							for (int j = 0; j < children.size(); j++) {
-							newans.push_back(make_pair(index, children[j]));
+						} else {
+							int maxlines = PKB::maxProgLines;
+							querycluster.pop_back();
+							vector<pair<int, int>> newans;
+					
+							for (int i = 0; i < maxlines; i++) {
+								STMT index = i + 1;
+								int followed = PKB::follows.getFollowsBy(index);
+								if (followed != -1) {
+									newans.push_back(make_pair(index, followed));
+								}
 							}
+							QueryProjectNode* newprojnode = new QueryProjectNode(newans);
+							querycluster.push_back(newprojnode);
 						}
-					}
-					QueryProjectNode* newprojnode = new QueryProjectNode(newans);
-					querycluster.push_back(newprojnode);
-				
-				break; }
-			
-			case QueryEnums::ParentStar: {
-	
-				QueryEnums::QueryVar firsttype = rnode->getFirstVariableType();
-				QueryEnums::QueryVar secondtype = rnode->getSecondVariableType();
-				
-				if (firsttype != QueryEnums::QueryVar::Stmt || secondtype != QueryEnums::QueryVar::Stmt) {
-					querycluster.pop_back();
-					vector<pair<string, string>> newans;
-					newans.push_back(make_pair("false", "false"));
-					QueryProjectNode* newprojnode = new QueryProjectNode(newans);
-					querycluster.push_back(newprojnode);
-				} else {
-					int maxlines = PKB::maxProgLines;
-					querycluster.pop_back();
-					vector<pair<int, int>> newans;
-					for (int i = 0; i < maxlines; i++) {
-						int index = i + 1;
-						vector<STMT> getchildrenstar = PKB::parent.getChildrenStar(index);
-						if (!getchildrenstar.empty()) {
-							for (int j = 0; j < getchildrenstar.size(); j++) {
-								newans.push_back(make_pair(index, getchildrenstar[j]));
+						break; 
+						
+					case QueryEnums::FollowsStar: 
+						if (firsttype != QueryEnums::QueryVar::Stmt || secondtype != QueryEnums::QueryVar::Stmt) {
+							querycluster.pop_back();
+							vector<pair<string, string>> newans;
+							newans.push_back(make_pair("false", "false"));
+							QueryProjectNode* newprojnode = new QueryProjectNode(newans);
+							querycluster.push_back(newprojnode);
+						} else {
+							int maxlines = PKB::maxProgLines;
+							querycluster.pop_back();
+							vector<pair<int, int>> newans;
+							for (int i = 0; i < maxlines; i++) {
+								int index = i + 1;
+								vector<STMT> getfollowsbystar = PKB::follows.getFollowsStarBy(index);
+								if (!getfollowsbystar.empty()) {
+									for (int j = 0; j < getfollowsbystar.size(); j++) {
+										newans.push_back(make_pair(index, getfollowsbystar[j]));
+									}
+								}
 							}
+							QueryProjectNode* newprojnode = new QueryProjectNode(newans);
+							querycluster.push_back(newprojnode);
 						}
+				
+						break; 
+					case QueryEnums::Parent: 
+						if (firsttype != QueryEnums::QueryVar::Stmt || secondtype != QueryEnums::QueryVar::Stmt) {
+							querycluster.pop_back();
+							vector<pair<string, string>> newans;
+							newans.push_back(make_pair("false", "false"));
+							QueryProjectNode* newprojnode = new QueryProjectNode(newans);
+							querycluster.push_back(newprojnode);
+						} else {
+							int maxlines = PKB::maxProgLines;
+							querycluster.pop_back();
+							vector<pair<int, int>> newans;
+							for (int i = 0; i < maxlines; i++) {
+								STMT index = i + 1;
+								vector<STMT> children = PKB::parent.getChildren(index);
+								if (!children.empty()) {
+									for (int j = 0; j < children.size(); j++) {
+									newans.push_back(make_pair(index, children[j]));
+									}
+								}
+							}
+							QueryProjectNode* newprojnode = new QueryProjectNode(newans);
+							querycluster.push_back(newprojnode);
+						}
+						break; 
+
+					case QueryEnums::ParentStar: 
+						if (firsttype != QueryEnums::QueryVar::Stmt || secondtype != QueryEnums::QueryVar::Stmt) {
+							querycluster.pop_back();
+							vector<pair<string, string>> newans;
+							newans.push_back(make_pair("false", "false"));
+							QueryProjectNode* newprojnode = new QueryProjectNode(newans);
+							querycluster.push_back(newprojnode);
+						} else {
+							int maxlines = PKB::maxProgLines;
+							querycluster.pop_back();
+							vector<pair<int, int>> newans;
+							for (int i = 0; i < maxlines; i++) {
+								int index = i + 1;
+								vector<STMT> getchildrenstar = PKB::parent.getChildrenStar(index);
+								if (!getchildrenstar.empty()) {
+									for (int j = 0; j < getchildrenstar.size(); j++) {
+										newans.push_back(make_pair(index, getchildrenstar[j]));
+									}
+								}
+							}
+							QueryProjectNode* newprojnode = new QueryProjectNode(newans);
+							querycluster.push_back(newprojnode);
 					}
-					QueryProjectNode* newprojnode = new QueryProjectNode(newans);
-					querycluster.push_back(newprojnode);
+					break; 
+					case QueryEnums::Uses: 
+						break; 
+					case QueryEnums::Modifies: 
+						break; 
 				}
-				break; }
-			case QueryEnums::Uses: {
-				break; }
-			case QueryEnums::Modifies: {
-				break; }
-			default: throw SPAException("Save me. I am just a default.");
 			}
 		}
 	}
 }
-}
-

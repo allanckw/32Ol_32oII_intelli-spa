969995180ed4ee7209e4216bb713e1578c2fac2a
neodeath <angnickolas@gmail.com>
2012-10-29 19:07:12 +0800

    setup laizo integration test enviroment

---
+++ $P@/SPA/IntegrationTesting/TestQueryParser.cpp
@@ -0,0 +1,26 @@
+#include "TestQueryParser.h"
+#include <cppunit/config/SourcePrefix.h>
+#include "../SPA/PKB.h"
+#include "../SPA/Parser.h"
+#include "../SPA/ASTNode.h"
+#include "../SPA/StmtLstNode.h"
+#include "../SPA/StmtNode.h"
+#include "../SPA/AssignmentParser.h"
+#include "../SPA/DesignExtractor.h"
+#include "../SPA/PKB.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION(TestQueryParser); // Note 4 
+
+
+TestQueryParser::TestQueryParser(void)
+{
+}
+
+
+TestQueryParser::~TestQueryParser(void)
+{
+}
+
+void TestQueryParser::TestQueryParserBuilder()
+{
+}

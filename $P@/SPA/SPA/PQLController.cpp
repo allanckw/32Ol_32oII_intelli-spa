2151ea9123606e04dfe1f52dd5258656105af24c
Allan Chong <kw_chong@nus.edu.sg>
2013-04-02 20:45:43 +0800

    Removed unwanted files

--- $P@/SPA/SPA/PQLController.cpp
+++ $P@/SPA/SPA/PQLController.cpp
@@ -1,7 +1,7 @@
 #include "PQLController.h"
 #include "MultiQueryEval.h"
 #include "PKB.h"
-
+#include "Dnf.h"
 /**
 * Public method for evaluating queries.
 * @param query query string
@@ -9,8 +9,13 @@
 */
 void PQLController::evaluateQuery(const string& query, list<string>& results)
 {
+	//uncomment for rerouting...
+	//int or = query.find("or");
+	//if(or >= 0) {
+//		Dnf::Eval(query, results);
+	//} else {
 	MultiQueryEval::evaluateQuery(query, results);
-
+	//}
 	PKB::next.tearDownCache();
 	PKB::affects.tearDownCache();
 }
--- lol.bat
+++ lol.bat
@@ -1,1 +1,25 @@
-start F:\3201_3202\SPA\$P@\SPA\Release\autotester.exe F:\3201_3202\SPA\source9.txt  F:\3201_3202\SPA\Queries9.1.txt  F:\3201_3202\SPA\out9.1.xml > NUL
+Call F:\3201_3202\SPA\$P@\SPA\Release\autotester.exe F:\3201_3202\SPA\source1.txt  F:\3201_3202\SPA\Queries1.txt  F:\3201_3202\SPA\out1.xml > NUL
+echo "Source 1 complete"
+
+Call   F:\3201_3202\SPA\$P@\SPA\Release\autotester.exe F:\3201_3202\SPA\source2.txt  F:\3201_3202\SPA\Queries2.txt  F:\3201_3202\SPA\out2.xml > NUL
+echo "Source 2 complete"
+
+Call   F:\3201_3202\SPA\$P@\SPA\Release\autotester.exe F:\3201_3202\SPA\source3.txt  F:\3201_3202\SPA\Queries3.txt  F:\3201_3202\SPA\out3.xml > NUL
+echo "Source 3 complete"
+
+Call  F:\3201_3202\SPA\$P@\SPA\Release\autotester.exe F:\3201_3202\SPA\source4.txt  F:\3201_3202\SPA\Queries4.txt  F:\3201_3202\SPA\out4.xml > NUL
+echo "Source 4 complete"
+
+Call  F:\3201_3202\SPA\$P@\SPA\Release\autotester.exe F:\3201_3202\SPA\source5.txt  F:\3201_3202\SPA\Queries5.txt  F:\3201_3202\SPA\out5.xml > NUL
+echo "Source 5 complete"
+
+Call  F:\3201_3202\SPA\$P@\SPA\Release\autotester.exe F:\3201_3202\SPA\source6.txt  F:\3201_3202\SPA\Queries6.txt  F:\3201_3202\SPA\out6.xml > NUL
+echo "Source 6 complete"
+
+Call  F:\3201_3202\SPA\$P@\SPA\Release\autotester.exe F:\3201_3202\SPA\source9.txt  F:\3201_3202\SPA\Queries9.1.txt  F:\3201_3202\SPA\out9.1.xml > NUL
+
+Call   F:\3201_3202\SPA\$P@\SPA\Release\autotester.exe F:\3201_3202\SPA\source9.txt  F:\3201_3202\SPA\Queries9.1.txt  F:\3201_3202\SPA\out9.2.xml > NUL
+echo "Source 9 complete"
+
+Call  F:\3201_3202\SPA\$P@\SPA\Release\autotester.exe F:\3201_3202\SPA\source10.txt  F:\3201_3202\SPA\Queries10.txt  F:\3201_3202\SPA\out10.xml > NUL
+echo "Source 10 complete"
